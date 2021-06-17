075c684f5dd7b9cc99217c9ec5a7fefa824ced38
Christwide Oscar <Aviliusedme@gmail.com>
2021-06-16 20:10:43 -0500

    Body Contraints

---
+++ hours/lib/body.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Container(
+                    width: 250,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
