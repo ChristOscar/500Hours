e60126b547f0ac4879931b33c38a7832e920e91b
Christwide Oscar <Aviliusedme@gmail.com>
2021-06-21 13:43:40 -0500

    Removale

--- hours/lib/constrants.dart
+++ hours/lib/constrants.dart
@@ -5,5 +5,6 @@ const kPrimaryColor = Color(0xFF0C9869);
 const kTextColor = Color(0xFF3C4046);
 const kBackgroundColor = Color(0xFFF9F8FD);
 const kOranageColors = Color(0xFFFF7043);
+const ktoranageColors = Color(0xFFFF7043);
 
 const double kDefaultPadding = 20.0;
