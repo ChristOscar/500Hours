ad9a92f238637124abccc5d6dc8e277e45f7e510
Christwide Oscar <Aviliusedme@gmail.com>
2021-06-21 13:51:06 -0500

    Layout of the app - Reupload

---
+++ hours/lib/screens/home/componets/header.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:hours/constrants.dart';
+
+class Header extends StatelessWidget {
+  const Header({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
+      height: 150,
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 36 + kDefaultPadding,
+            ),
+            height: 150,
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
+              ),
+            ),
+            child: Row(
+              children: [
+                Text(
+                  "Hello User ", //User impeltmentation will be here
+                  style: Theme.of(context).textTheme.headline5.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                Spacer(),
+                Image.asset("hours/assets/images/logo.png")
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
